af03679464ff3475749e95d4f680ab8450fe0097
Marcelo Del-Rei <assesci@outlook.com>
2021-10-06 12:33:10 -0300

    Teste de Api

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const PORT  = process.env.PORT || 8877;
+
+app.get('/equips', (req, res) => {
+    res.json({
+        cpu: 'AMD Ryzen 1700',
+        gpu: 'Galax GTX 1070',
+        mouse: 'Logitech G PRO',
+        headset: 'Turtle beach PX22',
+        keayboard: 'Alfawise v1',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.json({
+        name: 'Assesci',
+        email: 'assesci@outlook.com',
+        urls: [
+            {
+                type: 'github',
+                url: `http://github.com/assesci`
+            },
+        ]
+    })
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'OK'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('Escutando na porta: ' + PORT);
+})
